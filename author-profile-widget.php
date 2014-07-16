80ca58ad94639effc3ab47690d8d2bf3bacb8485
Takashi Kitajima <inc@2inc.org>
2014-07-16 15:59:58 +0900

    Change license notation

--- author-profile-widget.php
+++ author-profile-widget.php
@@ -10,22 +10,8 @@
  * Modified:
  * Text Domain: author-profile-widget
  * Domain Path: /languages/
- * License: GPL2
- *
- * Copyright 2014 Takashi Kitajima (email : inc@2inc.org)
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2, as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * License: GPLv2
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
 $Author_Profile_Widget = new Author_Profile_Widget();
 
